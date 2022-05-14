dd2cd7a60f0dcfbff945da04ce38f667118c6ce7
YUseiDoi <yusedo1101@gmail.com>
2022-05-15 00:48:09 +0900

    2022/05/15 Arrays and Strings Number2

--- Chapter-1_Arrays-and-Strings/2_Check-Permutation/CheckPermutation_myAnswer.cpp
+++ Chapter-1_Arrays-and-Strings/2_Check-Permutation/CheckPermutation_myAnswer.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-bool checkPermutation(vector<string> targetStrings);		// check permutation
+bool checkPermutation(vector<vector<char>> targetStrings);		// check permutation
 
 int main()
 {
-	vector<string> targetStrings = {"HelloBoys", "BoHellyso"};
+	vector<vector<char>> targetStrings = {"HelloBoys", "BoHellyso"};
 	cout << checkPermutation(targetStrings) << endl;
 	return 0;
 }
 
-bool checkPermutation(vector<string> targetStrings)
+bool checkPermutation(vector<vector<char>> targetStrings)
 {
-	vector<vector<char>> permutationArray = {};		// set 2-d array to check permutation
+	vector<vector<char>> permutationArray = {};		// set 2-d array to check permutatio
 	int i, j;
-	for(i = 0; i < targetStrings.size(); i++)
+
+	if (targetStrings[0].size() == targetStrings[1].size()) return false;	// if the lengths of both strings are diffrent, returns false
+
+	for(i = 0; i < 2; i++)
 	{
-		for(j = 0; j < targetStrings[i].length(); j++)
+		for(j = 0; j < targetStrings[i].size(); j++)
 		{
-			permutationArray.at(i)
+			permutationArray[i].at(targetStrings[i].at(j))++;
 		}
 	}
 }
